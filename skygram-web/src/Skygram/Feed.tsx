9bacd56020d1bcda6ae080001f48591350ff1d85
josh <jpollock412@gmail.com>
2024-11-08 16:21:00 -0500

    start using virtual

--- skygram-web/src/Skygram/Feed.tsx
+++ skygram-web/src/Skygram/Feed.tsx
@@ -1,6 +1,7 @@
 import { Agent } from "@atproto/api";
 import { LoaderCircle } from "lucide-react";
 import { Fragment } from "react";
+import Virtual from "../components/Virtual";
 import Post from "./Post";
 import useFeed from "./useFeed";
 export default function Feed({did,rkey,agent}:{
@@ -8,19 +9,28 @@ export default function Feed({did,rkey,agent}:{
     rkey:string,
     agent: Agent
 }) {
-    const {postIndexes,getPost } = useFeed({did,rkey,agent})
+    const {postIndexes,getPost,postCount } = useFeed({did,rkey,agent})
     return (
         <>
-        {postIndexes && postIndexes.length ? postIndexes.map((postIndex)=>{
-          const post = getPost(parseInt(postIndex));
-          return (
-            <Fragment key={postIndex}>
-              {post ? <Post {...post}/> :null}
-            </Fragment>
-          )
-        }):<LoaderCircle
-          className="animate-spin h-5 w-5 mr-3 ..."
-        />}
-      </>
+
+            <>
+            {postIndexes && postIndexes.length ? (
+                <>
+                    <Virtual
+                        count={postCount}
+                        Item={({index})=> {
+                            const post = getPost(index);
+                            return <Fragment key={index}>
+                                {post ? <Post {...post}/> :null}
+                            </Fragment>
+                        }}
+                    />
+                </>
+            ):<LoaderCircle
+                className="animate-spin h-5 w-5 mr-3 ..."
+                />}
+            </>
+
+        </>
     )
 }
