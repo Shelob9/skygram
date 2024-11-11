56fa118d944b614d44d7c4c865c0931f08117cac
josh <jpollock412@gmail.com>
2024-11-11 16:44:32 -0500

    Use virtualizer

--- skygram-web/src/Skygram/Feed.tsx
+++ skygram-web/src/Skygram/Feed.tsx
@@ -1,7 +1,7 @@
 import { Agent } from "@atproto/api";
+import { useVirtualizer } from "@tanstack/react-virtual";
 import { LoaderCircle } from "lucide-react";
-import { Fragment } from "react";
-import Virtual from "../components/Virtual";
+import { Fragment, useRef } from "react";
 import Post from "./Post";
 import useFeed from "./useFeed";
 export default function Feed({did,rkey,agent}:{
@@ -10,27 +10,42 @@ export default function Feed({did,rkey,agent}:{
     agent: Agent
 }) {
     const {postIndexes,getPost,postCount } = useFeed({did,rkey,agent})
-    return (
-        <>
+    const parentRef = useRef<HTMLDivElement>(null)
+    const rowVirtualizer = useVirtualizer({
+        count: postCount,
+        getScrollElement: () => parentRef.current,
+        estimateSize: () => 35,
+        overscan: 5,
+    })
 
-            <>
+    return (
+        <div>
+            <div
+                ref={parentRef}
+                className="w-full overflow-auto"
+            >
             {postIndexes && postIndexes.length ? (
-                <>
-                    <Virtual
-                        count={postCount}
-                        Item={({index})=> {
-                            const post = getPost(index);
-                            return <Fragment key={index}>
-                                {post ? <Post {...post}/> :null}
+
+                <div>
+                    {rowVirtualizer.getVirtualItems().map((virtualRow) => (
+                        <div
+                            key={virtualRow.index}
+                            className={virtualRow.index % 2 ? 'ListItemOdd' : 'ListItemEven'}
+                        >
+                            <Fragment key={virtualRow.index}>
+                                <>
+                                    {getPost(virtualRow.index) ? <Post {...getPost(virtualRow.index)}/> :null}
+                                </>
                             </Fragment>
-                        }}
-                    />
-                </>
+
+
+                        </div>
+                    ))}
+                </div>
             ):<LoaderCircle
                 className="animate-spin h-5 w-5 mr-3 ..."
                 />}
-            </>
-
-        </>
+            </div>
+        </div>
     )
 }
--- skygram-web/src/main.tsx
+++ skygram-web/src/main.tsx
@@ -1,29 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { AuthProvider } from './BskyAuth/AuthProvider.tsx'
-import { ENV, HANDLE_RESOLVER_URL, PLC_DIRECTORY_URL } from './BskyAuth/constants.ts'
 import './index.css'
-const clientId = `http://localhost?${new URLSearchParams({
-  scope: 'atproto transition:generic',
-  redirect_uri: Object.assign(new URL(window.location.origin), {
-    hostname: 'http://localhost',
-    search: new URLSearchParams({
-      env: ENV,
-      handle_resolver: 'https://bsky.social',
-    }).toString(),
-  }).href,
-})}`
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider
-      clientId={clientId}
-      plcDirectoryUrl={PLC_DIRECTORY_URL}
-      handleResolver={HANDLE_RESOLVER_URL}
-      allowHttp={ENV === 'development' || ENV === 'test'}
-    >
+
       <App />
-    </AuthProvider>
 
   </StrictMode>,
 )
