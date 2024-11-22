034c7ff54a422200b20b0ad0eade7a4d970d99b7
josh <jpollock412@gmail.com>
2024-11-22 12:50:39 -0500

    feedgen entry

--- skygram-web/vite.config.ts
+++ skygram-web/vite.config.ts
@@ -1,5 +1,6 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:5100',
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        feedgen: resolve(__dirname, 'feedgen.html'),
+      },
     },
   },
 })
